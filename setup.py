1297e52cd1b3774fe578480f65da3868207a6da2
nlhw <microGen@users.noreply.github.com>
2020-01-30 21:48:04 +0100

    Renamed GeoUtils3D to geoutils3D, created package around module

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="geoutils3D",
+    version="0.2.1",
+    author="N. Wichmann",
+    author_email="-", #T.B.D.
+    description="Utilities for 3D geometry operations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microGen/GeoUtils3D",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

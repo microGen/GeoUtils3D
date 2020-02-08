28c80585ead81815857b6fc6b41c87d898d77b25
nlhw <microGen@users.noreply.github.com>
2020-02-08 13:40:46 +0100

    Updated the readme file, made the Edge class independent of Line.

--- README.md
+++ README.md
@@ -1,28 +1,63 @@
 # GeoUtils3D
-Incomplete list of utility functions for 3D geometry operations. Will be updated irregularily.
+Compact packet of common 3D geometry calculations in Python 3.7+
 
 ## Basic Usage
-Primitives are implemented as named tuples. See Python documentation `collections.namedtuple`.
-Technically, points in 3D space can be expressed as vectors, but I chose to differentiate between points and vectors for sake of clarity. Points are thus named tuples and vectors Numpy arrays.
+There are three basic types of geometry classes:
+* Math types (mathematical geometry objects) found in `mathtypes.py`
+    * Point `Point`
+    * Line `Line`
+    * Plane `Plane`
+* Mesh types (3D mesh / solid geometry objects) found in `meshtypes.py`
+    * Vertex `Vertex`
+    * Edge `Edge`
+    * Face `Face`
+* UV types (strictly 2D geometry objects, e.g. for projections onto a 3D plane) found in `uvtypes.py`
+    * UV Point `UVPoint`
+    * UV Line `UVLine`
+    
+### No inheritance?
+Although it seems logical at first to let the Mesh and UV classes inherit their features from the Math classes,
+this leads to a lot of naming problems.
+Therefore, I limited the inheritance to cases where it makes sense.
 
-### 3D geometry
-The primitives point, line and plane are defined via coordinates in 3D space.
-Defining a point:
+## 3D geometry
+The primitives `Point`, `Line` and `Plane` are defined via coordinates in 3D space.
+Defining a `Point` is possible by using scalar coordinates or a vector (numpy array):
 
 `p0 = Point(x, y, z)`
 
-Using points, lines and planes can be defined:
+`p1 = Point(numpy.array([a,b,c]))`
 
-`l1 = Line(p0, p1)`
+Using points or vectors, lines and planes can be defined:
+
+`l0 = Line(p0, p1)`
+
+`pl0 = Plane(p0, numpy.array([a,b,c]), mumpy.array([u,v,w]))`
+
+Multiple representation forms are possible. These are converted internally to yield the same set of data.
 
 ### UV geometry
-It is also possible to define the same primitives in UV space:
+The same goes for UV geometry
 
-`pUV = PointUV(u, v)`
+`pUV = UVPoint(u, v)`
 
+However, since the `UVLine` only takes `UVPoint` or 2D-vectors, defining a `UVLine` object using 3D objects
+as arguments will result in an error.
+
+### Mesh geometry
+The difference between a `Vertex` and a `Point` is just in naming, to keep respective geometry types consistent.
+However, classes of higher orders exhibit greater differences: `Line` and `Plane` are infinite, whereas `Edge`
+and `Face` are delimited by vertices. An attempt to use 3D calculations meant for the mathematical types on
+mesh objects will result in errors.
+
+Defining and using mesh geometry objects is similar to math geometry objects.
+
+`Edge` will only take vectors or `Vertex` objects instead of `Point` objects.
+
+`Face` is still work in progress.
 
 ## Functions
-Functions usually take primitives or vectors as arguments.
+Functions usually take math types or vectors as arguments.
 
 ### General functions
 `calculate_normal(plane: Plane) -> np.ndarray`
@@ -39,7 +74,7 @@ Functions usually take primitives or vectors as arguments.
 `dist_point_plane(point: Point, plane: Plane) -> float`
 
 ### UV operations
-`map_xyz_to_uv(origin: Point, u_axis: np.ndarray, normal: np.ndarray, point: Point) -> PointUV`
+`map_xyz_to_uv(origin: Point, u_axis: np.ndarray, normal: np.ndarray, point: Point) -> UVPoint`
 
 `point_in_triangle(face_uv: PlaneUV, point_uv: PointUV) -> bool`
 
--- meshtypes.py
+++ meshtypes.py
@@ -11,7 +11,7 @@ from numpy import ndarray
 
 import utility
 from mathtypes import Point as __Point
-from mathtypes import Line as __Line
+#from mathtypes import Line as __Line
 from mathtypes import Plane as __Plane
 
 
@@ -19,16 +19,50 @@ class Vertex(__Point):
     """Vertex primitive in 3D space"""
     _dimension = 3
 
-class Edge(__Line):
+
+class Edge:
     """Edge primitive in 3D space"""
     _dimension = 3
-    _argtypes_point = [ndarray, Vertex]
+    _argtypes_vert = [ndarray, Vertex]
 
     def __init__(self, vert_0, vert_1):
-        utility.argcheck_type(self._argtypes_point, vert_0)
-        utility.argcheck_type(self._argtypes_point, vert_1)
+        """Creates Edge
+        ARGS:
+            vert_0: Vertex object or ndarray representing vector to vertex
+            vert_1: Vertex object or ndarray representing vector to vertex
+        """
+        utility.argcheck_type(self._argtypes_vert, vert_0)
+        utility.argcheck_type(self._argtypes_vert, vert_1)
         utility.argcheck_dim(self._dimension, vert_0, vert_1)
-        super(Edge, self).__init__(vert_0, vert_1, 'point')
+        self.__vertex_a = utility.vec(vert_0)
+        self.__vertex_b = utility.vec(vert_1)
+        self.__vector = self.__vertex_b - self.__vertex_a
+
+    @property
+    def vertex_a(self):
+        return Vertex(self.__vertex_a)
+
+    @vertex_a.setter
+    def vertex_a(self, new_vert):
+        utility.argcheck_type(self._argtypes_vert, new_vert)
+        utility.argcheck_dim(self._dimension, new_vert)
+        self.__vertex_a = new_vert
+        self.__vector = self.__vertex_b - self.__vertex_a
+
+    @property
+    def vertex_b(self):
+        return Vertex(self.__vertex_b)
+
+    @vertex_b.setter
+    def vertex_b(self, new_vert):
+        utility.argcheck_type(self._argtypes_vert, new_vert)
+        utility.argcheck_dim(self._dimension, new_vert)
+        self.__vertex_b = new_vert
+        self.__vector = self.__vertex_b - self.__vertex_a
+
+    @property
+    def vector(self):
+        return self.__vector
 
     def point(self, proportion: float):
         """Calculates a point on the edge.
@@ -40,8 +74,9 @@ class Edge(__Line):
         """
         utility.argcheck_type([int, float], proportion)
         utility.argcheck_minmax(0, 1, proportion)
-        point_on_edge = self.point_a + self.vector * proportion
+        point_on_edge = self.__vertex_a + self.vector * proportion
         return point_on_edge
+
 
 class Face(__Plane):
     """Face primitive in 3D space"""
@@ -50,4 +85,21 @@ class Face(__Plane):
     # edges connect counter-clockwise
     # use edges to find neighboring face
     _dimension = 3
-    _argtypes_point = [ndarray, Vertex]
+    _argtypes_vert = [ndarray, Vertex]
+
+    def __init__(self, element_0, element_1, element_2 = None):
+        if element_2 is not None:
+            # Vertex initialization mode
+            utility.argcheck_type(self._argtypes_vert, element_0)
+            utility.argcheck_type(self._argtypes_vert, element_1)
+            utility.argcheck_type(self._argtypes_vert, element_2)
+        else:
+            if type(element_0) == Edge and type(element_1) == Edge:
+                # Two Edge initialization mode
+                pass
+            else:
+                # Vertex and Edge initialization mode. End vertices of edge must be distinct from passed vertex.
+                args = [element_0, element_1]
+                arg_edge = next(filter(lambda a: type(a) == Edge, args))
+                arg_vert = next(filter(lambda a: type(a) != Edge, args))
+                utility.argcheck_type(self._argtypes_vert, arg_vert)
